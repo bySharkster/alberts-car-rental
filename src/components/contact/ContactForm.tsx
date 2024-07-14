3768db3395a7b06e2174279205098afe897dc2ee
Fernando Aponte <fernandoaponte0609@gmail.com>
2024-07-14 18:15:53 -0400

    Form Text Minor Fix

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 
 export default function ContactForm() {
   return (
-    <div className="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8 dark:border-gray-700 bg-gray-900">
-      <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
+    <div className="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8 opacity-95 dark:border-gray-700 bg-gray-900">
+      <h2 className="text-xl font-semibold text-white">
         Información de Contacto
       </h2>
       <div className="my-8">
         <div className="border-t border-gray-300"></div>
       </div>
-      <p>
+      <p className="text-white">
         Para mas informacion, llamanos al {config.ownerPhone} o contactanos por{" "}
         <Link
           href={"https://wa.link/k91t18"}
